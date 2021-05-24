48b4195a835a4709e2f9bc23d285bbc02e14b3df
javaspence <54484567+javaspence@users.noreply.github.com>
2021-05-24 12:00:27 -0500

    Added function "upgrade()"

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ var bananas = 0;
 var click = 1;
 function increment(){
 	bananas = bananas + click;
-	counter.innerHTML = bananas;
+	document.getElementById("counter").innerHTML = bananas;
 } 
+function upgrade(){
+  if (click < 2){
+    if (bananas < 100){
+      window.alert("not enough bananas, my g")
+    }
+    
+    if (bananas > 100){
+      window.confirm("you sure, broski?")
+      bananas = bananas - 100;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curclickpwr").innerHTML = "2"
+      document.getElementById("nextclickpwr").innerHTML =
+      "5"
+      document.getElementById("nextclickcost").innerHTML = "1000"
+      click = 2;
+    }
+  }
+}
