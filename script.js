36164b3e1e2b963fb388f7ff47cdf0351bc2a59a
javaspence <54484567+javaspence@users.noreply.github.com>
2021-05-28 14:56:27 -0500

    make upgrade() finally work (after 4 days)

--- script.js
+++ script.js
@@ -1,71 +1,48 @@
 var bananas = 0;
 var click = 1;
-var frick = ["you don't have cash money broski", "you have low cash money", "not enough cash money dude", "your cash money account is too dry"]
-// var autoclick = 0;
-function random_item(frick)
-{
-  
-return frick[Math.floor(Math.random()*frick.length)];
-     
-}
 function increment(){
 	bananas = bananas + click;
 	document.getElementById("counter").innerHTML = bananas;
 } 
 function upgrade(){
-  if (click < 2){
-    if (bananas < 100){
-      window.alert(random_item(frick))
-    }
-    
-    if (bananas > 100){
-      // window.confirm("you sure, broski?")
-      bananas = bananas - 100;
-      document.getElementById("counter").innerHTML = bananas;
-      document.getElementById("curclickpwr").innerHTML = "2"
-      document.getElementById("nextclickpwr").innerHTML =
-      "5"
-      document.getElementById("nextclickcost").innerHTML = "1000"
-      click = 2;
-    }
-  }
-  if (click > 2){
-    if (click < 5){
-      if (bananas < 1000){
-        window.alert(random_item(frick))
+  if (click == 1){
+      if (bananas > 100){
+        bananas = bananas - 100;
+        document.getElementById("counter").innerHTML = bananas;
+        document.getElementById("curclickpwr").innerHTML = "2";
+        document.getElementById("nextclickpwr").innerHTML = "5";
+        document.getElementById("nextclickcost").innerHTML = "1000";
+        click = 2;
       }
     }
   }
-}
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
+  if (click == 2){
+    if (bananas > 1000){
+      bananas = bananas - 1000;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curclickpwr").innerHTML = "5";
+      document.getElementById("nextclickpwr").innerHTML = "10";
+      document.getElementById("nextclickcost").innerHTML = "2500";
+      click = 5;
     }
   }
-}
-/* function autoclick(){
-  while (true){
-    sleep(1000);
-    bananas = bananas + autoclick;
-    document.getElementById("counter").innerHTML = bananas;
+  if (click == 5){
+    if (bananas > 2500){
+      bananas = bananas - 2500;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curclickpwr").innerHTML = "10";
+      document.getElementById("nextclickpwr").innerHTML = "20";
+      document.getElementById("nextclickcost").innerHTML = "5500";
+      click = 10;
+    }
   }
-}
-function upgradeAutoclick(){
-  if (bananas < 100){
-    window.alert("no cash money my dude");
+  if (click == 10){
+    if (bananas > 5500){
+      bananas = bananas - 2500;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curclickpwr").innerHTML = "10";
+      document.getElementById("nextclickpwr").innerHTML = "20";
+      document.getElementById("nextclickcost").innerHTML = "5500";
+      click = 10;
+    }
   }
-  if (bananas > 100){
-    autoclick = 1;
-    bananas = bananas - 100;
-    document.getElementById("counter").innerHTML = bananas;
-    document.getElementById("curbps").innerHTML = "1";
-    document.getElementById("nextbps").innerHTML = "5";
-    document.getElementById("nextbpscost").innerHTML = "1000";
-  }
-}
-function autoclickmult(){
-  autoclick();
-  upgradeAutoclick();
-} */
