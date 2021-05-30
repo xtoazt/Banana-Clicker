2deb72a7b6c1cc97c75aac95eab6184a8d41c29e
javaspence <54484567+javaspence@users.noreply.github.com>
2021-05-29 19:27:58 -0500

    more upgrades but not all of them

--- script.js
+++ script.js
@@ -6,19 +6,18 @@ function increment(){
 } 
 function upgrade(){
   if (click == 1){
-      if (bananas > 100){
-        bananas = bananas - 100;
-        document.getElementById("counter").innerHTML = bananas;
-        document.getElementById("curclickpwr").innerHTML = "2";
-        document.getElementById("nextclickpwr").innerHTML = "5";
-        document.getElementById("nextclickcost").innerHTML = "1000";
-        click = 2;
-      }
+    if (bananas > 100){
+      bananas = bananas - 100;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curclickpwr").innerHTML = "2";
+      document.getElementById("nextclickpwr").innerHTML = "5";
+      document.getElementById("nextclickcost").innerHTML = "500";
+      click = 2;
     }
   }
   if (click == 2){
-    if (bananas > 1000){
-      bananas = bananas - 1000;
+    if (bananas > 500){
+      bananas = bananas - 500;
       document.getElementById("counter").innerHTML = bananas;
       document.getElementById("curclickpwr").innerHTML = "5";
       document.getElementById("nextclickpwr").innerHTML = "10";
@@ -38,11 +37,37 @@ function upgrade(){
   }
   if (click == 10){
     if (bananas > 5500){
-      bananas = bananas - 2500;
+      bananas = bananas - 5500;
       document.getElementById("counter").innerHTML = bananas;
-      document.getElementById("curclickpwr").innerHTML = "10";
-      document.getElementById("nextclickpwr").innerHTML = "20";
-      document.getElementById("nextclickcost").innerHTML = "5500";
-      click = 10;
+      document.getElementById("curclickpwr").innerHTML = "20";
+      document.getElementById("nextclickpwr").innerHTML = "50";
+      document.getElementById("nextclickcost").innerHTML = "10000";
+      click = 20;
     }
   }
+  if (click == 20){
+    if (bananas > 10000){
+      bananas = bananas - 10000;
+      document.getElementById("counter").innerHTML = 
+      bananas;
+      document.getElementById("curclickpwr").innerHTML = 
+      "50";
+      document.getElementById("nextclickpwr").innerHTML = 
+      "100";
+      document.getElementById("nextclickcost").innerHTML = "25000";
+      click = 50;
+    }
+  }
+  if (click == 50){
+    if (bananas > 25000){
+      bananas = bananas - 25000;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curclickpwr").innerHTML = 
+      "100";
+      document.getElementById("nextclickpwr").innerHTML = 
+      "200";
+      document.getElementById("nextclickcost").innerHTML = "50000";
+      click = 100;
+    }
+  }
+}
