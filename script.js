c3f52d8d094c57991423931b97bb415c86ab4c06
javaspence <54484567+javaspence@users.noreply.github.com>
2021-06-01 16:10:24 -0500

    add "dev()" and more upgrades for autoclick

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// variable "bananas" is the amount of bananas that have been harvested (is shown in <span> element next to "Bananas:")
+// variable "click" is the power that one click will get you (by "power" I mean like how many bananas harvested)
+// variable "autoclickPower" is how many bananas are harvested per second
 var bananas = 0;
 var click = 1;
 var autoclickPower = 0;
+// don't name a function click() because it's reserved (for something else in JS). I learned that the hard way
 function increment(){
 	bananas = bananas + click;
 	document.getElementById("counter").innerHTML = bananas;
 }
-
+// this function is click upgrades, I didn't make it clear when I named the function
 function upgrade(){
+  // make sure when comparing for equal to use "=="
   if (click == 1){
     if (bananas >= 100){
       bananas = bananas - 100;
+      // everything below changes <span> elements under the click upgrades button
       document.getElementById("counter").innerHTML = bananas;
       document.getElementById("curclickpwr").innerHTML = "2";
       document.getElementById("nextclickpwr").innerHTML = "5";
@@ -116,4 +122,36 @@ function upgrade(){
       click = 1000;
     }
   }
+}
+function upgradeAutoclick(){
+  if (autoclickPower == 0){
+    if (bananas >= 100){
+      bananas = bananas - 100;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curautoclick").innerHTML = "1";
+      document.getElementById("nextautoclick").innerHTML =
+      "5";
+      document.getElementById("nextautoclickcost").innerHTML = "500";
+      // Don't ever use "==" when setting a variable again!
+      autoclickPower = 1;
+    }
+  }
+  if (autoclickPower == 1){
+    if (bananas >= 500){
+      bananas = bananas - 500;
+      document.getElementById("counter").innerHTML = bananas;
+      document.getElementById("curautoclick").innerHTML = "5";
+      document.getElementById("nextautoclick").innerHTML =
+      "10";
+      document.getElementById("nextautoclickcost").innerHTML = "1050";
+      autoclickPower = 5;
+    }
+  }
+}
+function myFunction() {
+  setInterval(function(){ bananas = bananas + autoclickPower; document.getElementById("counter").innerHTML = bananas }, 1000);
+}
+// call this in the javascript console by pressing F12 and clicking on console and typing "dev();"
+function dev() {
+  bananas = bananas + 100000000000000000;
 }
